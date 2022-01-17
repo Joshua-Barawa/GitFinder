9e6ba277e7c61123e3e71c36c0c9ebcb22c339c1
Joshua-Barawa <joshua.barawa@student.moringaschool.com>
2022-01-17 14:03:14 +0300

    added input property

--- src/app/repository.ts
+++ src/app/repository.ts
@@ -1,3 +1,4 @@
+
 export class Repository {
     constructor(public name:string, public language:string, public forks:number, public watchers:number,
         public stargazers_count:number, public html_url:string){}
